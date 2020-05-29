5124a9a5deb3c3f7bf5fa9e737a18d684a4b9e71
Dino Fejzagic <dino.f@live.de>
2020-05-29 18:47:31 +0200

    Add tracked pose demo script

---
+++ XRTK.Examples/Assets/XRTK.Examples/Demos/HandControllerDemo/Scripts/TrackedPoseDemoStation.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.EventDatum.Input;
+using XRTK.Interfaces.InputSystem.Handlers;
+
+namespace XRTK.Examples.Demos.HandController
+{
+    /// <summary>
+    /// Demo script on how to handle changes in the recognized hand pose.
+    /// </summary>
+    public class TrackedPoseDemoStation : MonoBehaviour, IMixedRealityInputHandler<XRTK.Definitions.Controllers.Hands.HandData>
+    {
+        [SerializeField]
+        private TMPro.TextMeshPro leftHandPoseText = null;
+
+        [SerializeField]
+        private TMPro.TextMeshPro rightHandPoseText = null;
+
+        public void OnInputChanged(InputEventData<XRTK.Definitions.Controllers.Hands.HandData> eventData)
+        {
+            if (eventData.Handedness == Definitions.Utilities.Handedness.Left)
+            {
+                var trackedPose = eventData.InputData.TrackedPose;
+                var pose = trackedPose != null ? trackedPose.Id : "Unknown";
+                leftHandPoseText.text = $"Left Pose: {pose}";
+            }
+            else if (eventData.Handedness == Definitions.Utilities.Handedness.Right)
+            {
+                var trackedPose = eventData.InputData.TrackedPose;
+                var pose = trackedPose != null ? trackedPose.Id : "Unknown";
+                rightHandPoseText.text = $"Right Pose: {pose}";
+            }
+        }
+    }
+}
