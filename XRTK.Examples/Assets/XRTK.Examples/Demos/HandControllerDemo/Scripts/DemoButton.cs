04b4450e29553582dfc4d2743e7a46e2d7706940
Dino Fejzagic <dino.f@live.de>
2020-06-01 17:26:29 +0200

    First round of cleanup for hand demos

--- XRTK.Examples/Assets/XRTK.Examples/Demos/HandControllerDemo/Scripts/DemoButton.cs
+++ XRTK.Examples/Assets/XRTK.Examples/Demos/HandControllerDemo/Scripts/DemoButton.cs
@@ -2,11 +2,18 @@
 using UnityEngine.UI;
 using XRTK.EventDatum.Input;
 using XRTK.Interfaces.InputSystem.Handlers;
+using XRTK.SDK.Input.Handlers;
 
-public class DemoButton : MonoBehaviour, IMixedRealityPointerHandler
+public class DemoButton : BaseInputHandler, IMixedRealityPointerHandler
 {
     [SerializeField]
     private Button.ButtonClickedEvent onClick = null;
+
+    [SerializeField]
+    private Transform pressableTransform = null;
+
+    [SerializeField]
+    private Vector3 pressedOffset = new Vector3(0, 0, .005f);
 
     public void OnPointerClicked(MixedRealityPointerEventData eventData)
     {
@@ -15,11 +22,11 @@ public class DemoButton : MonoBehaviour, IMixedRealityPointerHandler
 
     public void OnPointerDown(MixedRealityPointerEventData eventData)
     {
-
+        pressableTransform.localPosition = pressedOffset;
     }
 
     public void OnPointerUp(MixedRealityPointerEventData eventData)
     {
-
+        pressableTransform.localPosition = Vector3.zero;
     }
 }
