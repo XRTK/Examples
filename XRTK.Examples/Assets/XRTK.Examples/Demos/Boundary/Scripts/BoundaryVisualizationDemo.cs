3967363dcccac81fd0c82451794ae50cb304f77b
Stephen Hodgson <hodgson.designs@gmail.com>
2020-06-02 11:55:52 -0400

    updated boundary visualization demo and profile asset

--- XRTK.Examples/Assets/XRTK.Examples/Demos/Boundary/Scripts/BoundaryVisualizationDemo.cs
+++ XRTK.Examples/Assets/XRTK.Examples/Demos/Boundary/Scripts/BoundaryVisualizationDemo.cs
@@ -1,33 +1,28 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using XRTK.EventDatum.Boundary;
-using XRTK.Interfaces.BoundarySystem;
-using XRTK.Services;
+using System;
 using System.Collections.Generic;
-using XRTK.Utilities.Async;
 using UnityEngine;
-using UnityEngine.Experimental.XR;
-using XRTK.Extensions;
+using XRTK.Definitions.BoundarySystem;
+using XRTK.Services;
+using XRTK.Utilities.Async;
 
-namespace XRTK.Examples.Demos
+namespace XRTK.Examples.Demos.BoundarySystem
 {
     /// <summary>
     /// Demo class to show different ways of using the boundary system and visualizing the data.
     /// </summary>
-    public class BoundaryVisualizationDemo : MonoBehaviour, IMixedRealityBoundaryHandler
+    public class BoundaryVisualizationDemo : MonoBehaviour
     {
         private GameObject markerParent;
         private readonly List<GameObject> markers = new List<GameObject>();
 
         [SerializeField]
+        private bool showBoundary = true;
+
+        [SerializeField]
         private bool showFloor = true;
-
-        [SerializeField]
-        private bool showPlayArea = true;
-
-        [SerializeField]
-        private bool showTrackedArea = true;
 
         [SerializeField]
         private bool showBoundaryWalls = true;
@@ -39,14 +34,12 @@ namespace XRTK.Examples.Demos
 
         private void Awake()
         {
-            markerParent = new GameObject();
-            markerParent.name = "Boundary Demo Markers";
+            markerParent = new GameObject("Boundary Demo Markers");
             markerParent.transform.parent = MixedRealityToolkit.CameraSystem.MainCameraRig.PlayspaceTransform;
         }
 
         private void Start()
         {
-
             if (MixedRealityToolkit.BoundarySystem != null)
             {
                 if (markers.Count == 0)
@@ -56,40 +49,46 @@ namespace XRTK.Examples.Demos
             }
         }
 
-        private void Update()
-        {
-            if (MixedRealityToolkit.BoundarySystem != null)
-            {
-                MixedRealityToolkit.BoundarySystem.ShowFloor = showFloor;
-                MixedRealityToolkit.BoundarySystem.ShowPlayArea = showPlayArea;
-                MixedRealityToolkit.BoundarySystem.ShowTrackedArea = showTrackedArea;
-                MixedRealityToolkit.BoundarySystem.ShowBoundaryWalls = showBoundaryWalls;
-                MixedRealityToolkit.BoundarySystem.ShowBoundaryCeiling = showBoundaryCeiling;
-            }
-        }
-
         private async void OnEnable()
         {
             await new WaitUntil(() => MixedRealityToolkit.BoundarySystem != null);
-            MixedRealityToolkit.BoundarySystem.Register(gameObject);
+            MixedRealityToolkit.BoundarySystem.BoundaryProximityAlert += OnBoundaryProximityAlert;
+            MixedRealityToolkit.BoundarySystem.ShowBoundary = showBoundary;
+            MixedRealityToolkit.BoundarySystem.ShowFloor = showFloor;
+            MixedRealityToolkit.BoundarySystem.ShowWalls = showBoundaryWalls;
+            MixedRealityToolkit.BoundarySystem.ShowCeiling = showBoundaryCeiling;
         }
 
         private void OnDisable()
         {
-            MixedRealityToolkit.BoundarySystem?.Unregister(gameObject);
+            if (MixedRealityToolkit.BoundarySystem != null)
+            {
+                MixedRealityToolkit.BoundarySystem.BoundaryProximityAlert -= OnBoundaryProximityAlert;
+            }
         }
 
         #endregion MonoBehaviour Implementation
 
-        #region IMixedRealityBoundaryHandler Implementation
-
-        /// <inheritdoc />
-        public void OnBoundaryVisualizationChanged(BoundaryEventData eventData)
+        private void OnBoundaryProximityAlert(GameObject trackedGameObject, ProximityAlert proximityAlert)
         {
-            Debug.Log("[BoundaryVisualizationDemo] Boundary visualization changed.");
+            switch (proximityAlert)
+            {
+                case ProximityAlert.Clear:
+                    Debug.Log($"{gameObject.name} is safely inside the boundary.");
+                    break;
+                case ProximityAlert.Touch:
+                    Debug.LogWarning($"{gameObject.name} is approaching the boundary!");
+                    break;
+                case ProximityAlert.Exit:
+                    Debug.LogError($"{gameObject.name} has left the boundary!");
+                    break;
+                case ProximityAlert.Enter:
+                    Debug.Log($"{gameObject.name} has entered the boundary.");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(proximityAlert), proximityAlert, null);
+            }
         }
-
-        #endregion IMixedRealityBoundaryHandler Implementation
 
         /// <summary>
         /// Displays the boundary as an array of spheres where spheres in the
@@ -99,17 +98,9 @@ namespace XRTK.Examples.Demos
         {
             // Get the rectangular bounds.
 
-            if (!MixedRealityToolkit.BoundarySystem.TryGetRectangularBoundsParams(out Vector2 centerRect, out float angleRect, out float widthRect, out float heightRect))
+            if (!MixedRealityToolkit.BoundarySystem.TryGetRectangularBoundsParams(out var centerRect, out var angleRect, out var widthRect, out var heightRect))
             {
                 // If we have no boundary manager or rectangular bounds we will show no indicators
-                return;
-            }
-
-            var visualizationProfile = MixedRealityToolkit.Instance.ActiveProfile.BoundaryVisualizationProfile;
-
-            if (visualizationProfile.IsNull())
-            {
-                // We do not have a visualization profile configured, therefore do not render the indicators.
                 return;
             }
 
@@ -118,37 +109,25 @@ namespace XRTK.Examples.Demos
             const float indicatorScale = 0.1f;
             const float dimension = indicatorCount * indicatorDistance;
 
-            Vector3 center = new Vector3(centerRect.x, 0f, centerRect.y);
-            Vector3 corner = center - (new Vector3(dimension, 0.0f, dimension) * 0.5f);
+            var center = new Vector3(centerRect.x, 0f, centerRect.y);
+            var corner = center - (new Vector3(dimension, 0.0f, dimension) * 0.5f);
 
             corner.y += 0.05f;
+
             for (int xIndex = 0; xIndex < indicatorCount; ++xIndex)
             {
                 for (int yIndex = 0; yIndex < indicatorCount; ++yIndex)
                 {
-                    Vector3 offset = new Vector3(xIndex * indicatorDistance, 0.0f, yIndex * indicatorDistance);
-                    Vector3 position = corner + offset;
+                    var offset = new Vector3(xIndex * indicatorDistance, 0.0f, yIndex * indicatorDistance);
+                    var position = corner + offset;
 
-                    Material material = null;
-                    // Check inscribed rectangle first
-                    if (MixedRealityToolkit.BoundarySystem.Contains(position, Boundary.Type.PlayArea))
+                    if (MixedRealityToolkit.BoundarySystem.IsInsideBoundary(position))
                     {
-                        material = visualizationProfile.PlayAreaMaterial;
-                    }
-                    // Then check geometry
-                    else if (MixedRealityToolkit.BoundarySystem.Contains(position, Boundary.Type.TrackedArea))
-                    {
-                        material = visualizationProfile.TrackedAreaMaterial;
-                    }
-
-                    if (material != null)
-                    {
-                        GameObject marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                        var marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                         marker.name = "Boundary Demo Marker";
-                        marker.transform.parent = markerParent.transform;
+                        marker.transform.SetParent(markerParent.transform, false);
                         marker.transform.position = position;
                         marker.transform.localScale = Vector3.one * indicatorScale;
-                        marker.GetComponent<MeshRenderer>().sharedMaterial = material;
                         markers.Add(marker);
                     }
                 }
