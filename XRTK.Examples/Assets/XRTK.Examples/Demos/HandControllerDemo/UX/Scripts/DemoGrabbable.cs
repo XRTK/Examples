428821b224341716ef630871671b7c7a4ce43261
Dino Fejzagic <dino.f@live.de>
2020-06-17 14:56:13 +0200

    Fox grabbable demo

--- XRTK.Examples/Assets/XRTK.Examples/Demos/HandControllerDemo/UX/Scripts/DemoGrabbable.cs
+++ XRTK.Examples/Assets/XRTK.Examples/Demos/HandControllerDemo/UX/Scripts/DemoGrabbable.cs
@@ -11,16 +11,56 @@ using XRTK.Services;
 
 namespace XRTK.Examples.Demos.HandController.UX
 {
+    /// <summary>
+    /// A simple grabbable interaciton handler component. This component was just
+    /// created for demo purposes and is not part of the XRTK SDK. Feel free to use it as a starting
+    /// point for your own component until XRTK has a proper component for it.
+    /// </summary>
+    /// <remarks>This component is not optimized for production use.</remarks>
     public class DemoGrabbable : BaseInputHandler, IMixedRealityInputHandler, IMixedRealityInputHandler<MixedRealityPose>
     {
         private bool isGripped;
+        private float lerpTime;
         private Handedness gripHandedness;
+        private bool lerpBackToStart;
+        private Vector3 startPosition;
+        private Quaternion startRotation;
 
         [SerializeField]
         private MixedRealityInputAction grabAction = MixedRealityInputAction.None;
 
         [SerializeField]
         private MixedRealityInputAction gripPoseAction = MixedRealityInputAction.None;
+
+        [SerializeField]
+        [Tooltip("If the distance to the grabbed object is above the threshold it will lerp towards the grab pose.")]
+        private float lerpDistanceThreshold = .05f;
+
+        [SerializeField]
+        [Tooltip("Grab pose lerp duration.")]
+        private float lerpDuration = 1f;
+
+        [SerializeField]
+        [Tooltip("Grip pose offset for the object.")]
+        private Vector3 gripPositionOffset = Vector3.zero;
+
+        private void Awake()
+        {
+            startPosition = transform.position;
+            startRotation = transform.rotation;
+        }
+
+        private void Update()
+        {
+            if (lerpBackToStart)
+            {
+                var fraction = lerpTime / lerpDuration;
+                transform.position = Vector3.Slerp(transform.position, startPosition, fraction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, startRotation, fraction);
+                lerpBackToStart = fraction < 1f;
+                lerpTime += Time.deltaTime;
+            }
+        }
 
         public void OnInputChanged(InputEventData<MixedRealityPose> eventData)
         {
@@ -31,8 +71,18 @@ namespace XRTK.Examples.Demos.HandController.UX
 
             if (eventData.Handedness == gripHandedness && eventData.MixedRealityInputAction == gripPoseAction && isGripped)
             {
-                transform.position = eventData.InputData.Position;
-                transform.rotation = eventData.InputData.Rotation;
+                if (Vector3.Distance(transform.position, eventData.InputData.Position) <= lerpDistanceThreshold)
+                {
+                    transform.position = eventData.InputData.Position + eventData.InputData.Rotation * gripPositionOffset;
+                    transform.rotation = eventData.InputData.Rotation;
+                }
+                else
+                {
+                    var fraction = lerpTime / lerpDuration;
+                    transform.position = Vector3.Slerp(transform.position, eventData.InputData.Position + eventData.InputData.Rotation * gripPositionOffset, fraction);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, eventData.InputData.Rotation, fraction);
+                    lerpTime += Time.deltaTime;
+                }
             }
         }
 
@@ -54,6 +104,7 @@ namespace XRTK.Examples.Demos.HandController.UX
                     MixedRealityToolkit.InputSystem.Register(gameObject);
                 }
 
+                lerpTime = 0f;
                 eventData.Use();
             }
         }
@@ -70,6 +121,9 @@ namespace XRTK.Examples.Demos.HandController.UX
                 isGripped = false;
                 MixedRealityToolkit.InputSystem?.Unregister(gameObject);
                 eventData.Use();
+
+                lerpTime = 0f;
+                lerpBackToStart = true;
             }
         }
     }
