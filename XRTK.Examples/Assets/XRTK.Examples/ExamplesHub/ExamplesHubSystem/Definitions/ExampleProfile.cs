faf3dc83ee04187dbefbab2ccebd0cea96fb0a11
Dino Fejzagić <dino.f@live.de>
2021-09-04 19:27:31 +0200

    Locomotion System Example Stage (#47)
    * Update profiles
    
    * breaking changes from the core system refactor
    
    * removed the core system accessors from the xrtk service locator
    
    * renamed boundary system profile asset
    
    updated system configuration list
    
    * updated root profile serialization
    
    * Resolve merge issues
    
    * Fix demo profile locomotion system registration
    
    * Fix namespace compiler error in spatial awareness?
    
    * Update demo root profile to use new locomotion system
    
    * Assign default teleportation provider for demo profile
    
    * Adjust naming and namespaces
    
    * Set default movement provider
    
    * Update default demo profile
    
    * Update profile
    
    * Update demo profile
    
    * Update demo profile
    
    * Update demo profile
    
    * Update profiles
    
    * Update locomotion demo stage
    
    * Finalize demo stage
    
    * Implement ExamplesHubSystem
    
    * Add exit demo button to locomotion demo
    
    * Setup locomotion example with new examples hub system
    
    * Remove on rails provider
    
    * Minor improvement to example stage
    
    * Update demo UI appearance
    
    * Increase lerp time for solver
    
    * Make examples configurable via a profile
    
    * Improvements to examples
    
    * Rename assets for hotspot
    
    * Update editor
    
    * Update dependencies
    
    * Revert namespace changes
    
    * updated package dependencies
    
    * updated xr settings
    
    * updated packages
    
    Co-authored-by: Stephen Hodgson <sjh@emerge.io>
    Co-authored-by: Stephen Hodgson <hodgson.designs@gmail.com>

---
+++ XRTK.Examples/Assets/XRTK.Examples/ExamplesHub/ExamplesHubSystem/Definitions/ExampleProfile.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using XRTK.Definitions;
+using XRTK.Interfaces;
+
+namespace XRTK.Examples.ExamplesHub.Definitions
+{
+    /// <summary>
+    /// Configuration profile for an XRTK example.
+    /// </summary>
+    [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Demos/Example Profile", fileName = "ExampleProfile", order = 99)]
+    public class ExampleProfile : BaseMixedRealityProfile
+    {
+        [SerializeField]
+        [Tooltip("The examples title.")]
+        private string title = null;
+
+        /// <summary>
+        /// The examples title.
+        /// </summary>
+        public string Title => title;
+
+        [SerializeField]
+        [Tooltip("Example content description.")]
+        private string description = null;
+
+        /// <summary>
+        /// Example content description.
+        /// </summary>
+        public string Description => description;
+
+        [SerializeField]
+        [Tooltip("The name of the scene containing the example content.")]
+        private string sceneName = null;
+
+        /// <summary>
+        /// The name of the scene containing the example content.
+        /// </summary>
+        public string SceneName => sceneName;
+
+        [SerializeField]
+        [Tooltip("An URL pointing to documentation related to this example.")]
+        private string docsUrl = null;
+
+        /// <summary>
+        /// An URL pointing to documentation related to this example.
+        /// </summary>
+        public string DocsUrl
+        {
+            get => docsUrl;
+            internal set => docsUrl = value;
+        }
+
+        [SerializeField]
+        [Tooltip("Platforms supported by this example.")]
+        private RuntimePlatformEntry platformEntries = new RuntimePlatformEntry();
+
+        private List<IMixedRealityPlatform> runtimePlatforms = null;
+        /// <summary>
+        /// Platforms supported by this example.
+        /// </summary>
+        public IReadOnlyList<IMixedRealityPlatform> RuntimePlatforms
+        {
+            get
+            {
+                if (runtimePlatforms == null ||
+                    runtimePlatforms.Count == 0 ||
+                    runtimePlatforms.Count != platformEntries?.RuntimePlatforms?.Length)
+                {
+                    runtimePlatforms = new List<IMixedRealityPlatform>();
+
+                    for (int i = 0; i < platformEntries?.RuntimePlatforms?.Length; i++)
+                    {
+                        var platformType = platformEntries.RuntimePlatforms[i]?.Type;
+
+                        if (platformType == null)
+                        {
+                            continue;
+                        }
+
+                        IMixedRealityPlatform platformInstance;
+
+                        try
+                        {
+                            platformInstance = Activator.CreateInstance(platformType) as IMixedRealityPlatform;
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogError(e);
+                            continue;
+                        }
+
+                        runtimePlatforms.Add(platformInstance);
+                    }
+                }
+
+                return runtimePlatforms;
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("Does this example work with transparent displays?")]
+        private bool worksWithTransparentDisplay = true;
+
+        /// <summary>
+        /// Does this example work with transparent displays?
+        /// </summary>
+        public bool WorksWithTransparentDisplay
+        {
+            get => worksWithTransparentDisplay;
+            internal set => worksWithTransparentDisplay = value;
+        }
+
+        [SerializeField]
+        [Tooltip("Does this example work with opaque displays?")]
+        private bool worksWithOpaqueDisplay = true;
+
+        /// <summary>
+        /// Does this example work with opaque displays?
+        /// </summary>
+        public bool WorksWithOpaqueDisplay
+        {
+            get => worksWithOpaqueDisplay;
+            internal set => worksWithOpaqueDisplay = value;
+        }
+    }
+}
---
+++ XRTK.Examples/Assets/XRTK.Examples/ExamplesHub/ExamplesHubSystem/ExamplesHubSystem.cs
@@ -0,0 +1,137 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using XRTK.Examples.ExamplesHub.Definitions;
+using XRTK.Examples.ExamplesHub.Interfaces;
+using XRTK.Services;
+
+namespace XRTK.Examples.ExamplesHub
+{
+    [System.Runtime.InteropServices.Guid("6191eb65-ec4c-434a-9a43-939fbc398582")]
+    public class ExamplesHubSystem : BaseSystem, IExamplesHubSystem
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="profile">System configuration profile.</param>
+        public ExamplesHubSystem(ExamplesHubSystemProfile profile)
+            : base(profile)
+        {
+            uiPrefab = profile.UIPrefab;
+            allExamples = profile.Examples;
+        }
+
+        private readonly GameObject uiPrefab;
+        private readonly IReadOnlyList<ExampleProfile> allExamples;
+        private Scene? currentExampleScene;
+
+        /// <summary>
+        /// Game object reference to the spawned instance of <see cref="uiPrefab"/>.
+        /// </summary>
+        private GameObject ExamplesUI { get; set; }
+
+        /// <inheritdoc />
+        public IReadOnlyList<ExampleProfile> SupportedExamples { get; private set; }
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            // When in editor we want to check if the developer has
+            // already loaded more than the base scene in which case we the system shouldn't do anything
+            // since the developer is probably working with a multi-scene setup for development.
+            if (Application.isEditor && SceneManager.sceneCount > 1)
+            {
+                return;
+            }
+
+            // Filter for examples supported by the current platform.
+            var compatibleExamples = new List<ExampleProfile>();
+            for (var i = 0; i < allExamples.Count; i++)
+            {
+                var example = allExamples[i];
+                for (int j = 0; j < example.RuntimePlatforms.Count; j++)
+                {
+                    var platform = example.RuntimePlatforms[j];
+                    if (platform.IsAvailable)
+                    {
+                        compatibleExamples.Add(example);
+                        break;
+                    }
+                }
+            }
+            SupportedExamples = compatibleExamples;
+
+            // Initialize examples hub UI and events.
+            ExamplesUI = Object.Instantiate(uiPrefab);
+            SceneManager.sceneLoaded += SceneManager_SceneLoaded;
+            SceneManager.sceneUnloaded += SceneManager_SceneUnloaded;
+        }
+
+        /// <inheritdoc />
+        public override void Destroy()
+        {
+            if (Application.isPlaying)
+            {
+                SceneManager.sceneLoaded -= SceneManager_SceneLoaded;
+                SceneManager.sceneUnloaded -= SceneManager_SceneUnloaded;
+            }
+
+            base.Destroy();
+        }
+
+        /// <inheritdoc />
+        public void LoadExample(ExampleProfile example)
+        {
+            ExamplesUI.SetActive(false);
+            SceneManager.LoadSceneAsync(example.SceneName, LoadSceneMode.Additive);
+        }
+
+        /// <inheritdoc />
+        public void ExitExample()
+        {
+            if (!currentExampleScene.HasValue)
+            {
+                return;
+            }
+
+            SceneManager.UnloadSceneAsync(currentExampleScene.Value.buildIndex);
+        }
+
+        private void SceneManager_SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.buildIndex == 0)
+            {
+                // We don't care for the base scene loading. The base scene will always load
+                // first when the application launches.
+                return;
+            }
+
+            Assert.IsTrue(mode == LoadSceneMode.Additive, $"Only {nameof(LoadSceneMode.Additive)} scene loading is allowed!");
+            currentExampleScene = scene;
+
+            // While we have an exmaple scene loaded we want the examples selection to disappear.
+            ExamplesUI.SetActive(false);
+        }
+
+        private void SceneManager_SceneUnloaded(Scene scene)
+        {
+            if (currentExampleScene.HasValue && scene.buildIndex == currentExampleScene.Value.buildIndex)
+            {
+                // The currently loaded example was unloaded. Return to example selection.
+                currentExampleScene = null;
+                ExamplesUI.SetActive(true);
+            }
+        }
+    }
+}
